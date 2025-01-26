85dd75ef06a8a47f5c2bbe9754585e10fabbb64e
khoa phung <110983505+dangkhoadoiqwe@users.noreply.github.com>
2025-01-26 13:29:22 +0700

    Get All Users account
    Get All Users account by Admin

--- KSMS.API/Controllers/AccountController.cs
+++ KSMS.API/Controllers/AccountController.cs
@@ -15,6 +15,19 @@ namespace KSMS.API.Controllers
         {
             _accountService = accountService;
         }
+        [Route("api/admin/GetAllUseraccount")]
+        [HttpGet]
+        public async Task<IActionResult> GetAccUsersByAdmin([FromQuery] int page = 1, [FromQuery] int size = 10)
+        {
+            if (page <= 0 || size <= 0)
+                return BadRequest("Page and size must be greater than 0.");
+
+            
+            var pagedUsers = await _accountService.GetPagedUsersAsync(page, size);
+
+            return Ok(pagedUsers);
+        }
+
         [HttpPut("{id:guid}")]
         public async Task<ActionResult> UpdateCurrentAccount(Guid id, [FromForm] UpdateAccountRequest updateAccountRequest)
         {
--- KSMS.Application/Services/IAccountService.cs
+++ KSMS.Application/Services/IAccountService.cs
@@ -1,8 +1,12 @@
 ﻿using KSMS.Domain.Dtos.Requests.Account;
+using KSMS.Domain.Dtos.Responses;
+using KSMS.Domain.Pagination;
 
 namespace KSMS.Application.Services;
 
 public interface IAccountService
 {
     Task UpdateAccount(Guid id, UpdateAccountRequest updateAccountRequest);
+
+    Task<IPaginate<UserResponse>> GetPagedUsersAsync(int page, int pageSize);
 }
