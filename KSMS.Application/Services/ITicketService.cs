22d62046b40613839f2977c48ee8a6e7e708a040
Châu Thiên Ân <chauthienan01072002@gmail.com>
2025-02-04 13:02:40 +0700

    Fix Verified QR

--- KSMS.Application/Services/ITicketService.cs
+++ KSMS.Application/Services/ITicketService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,6 @@ namespace KSMS.Application.Services
 {
     public interface ITicketService
     {
-        Task<bool> VerifyTicketIdAsync(Guid ticketId);
+        Task VerifyTicketIdAsync(ClaimsPrincipal claims, Guid qrCodeId);
     }
 }
--- KSMS.Infrastructure/Services/TicketService.cs
+++ KSMS.Infrastructure/Services/TicketService.cs
@@ -1,25 +1,55 @@
-﻿using KSMS.Application.Repositories;
+﻿using System.Security.Claims;
+using KSMS.Application.Extensions;
+using KSMS.Application.Repositories;
 using KSMS.Application.Services;
 using KSMS.Domain.Entities;
+using KSMS.Domain.Exceptions;
 using KSMS.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace KSMS.Infrastructure.Services
 {
-    public class TicketService : ITicketService
+    public class TicketService : BaseService<TicketService>,ITicketService
     {
-        private readonly IUnitOfWork<KoiShowManagementSystemContext> _unitOfWork;
-
-        public TicketService(IUnitOfWork<KoiShowManagementSystemContext> unitOfWork)
+        public TicketService(IUnitOfWork<KoiShowManagementSystemContext> unitOfWork, ILogger<TicketService> logger) : base(unitOfWork, logger)
         {
-            _unitOfWork = unitOfWork;
+        }
+        public async Task VerifyTicketIdAsync(ClaimsPrincipal claims, Guid qrCodeId)
+        {
+            var accountId = claims.GetAccountId();
+            var qrCode = await _unitOfWork.GetRepository<Qrcode>()
+                .SingleOrDefaultAsync(predicate: x => x.Id == qrCodeId,
+                    include: query =>
+                        query.Include(x => x.TicketOrderDetail)
+                            .ThenInclude(x => x.Ticket)
+                            .ThenInclude(x => x.Show));
+                
+            if (qrCode.IsActive == false)
+            {
+                throw new BadRequestException("QR Code is used");
+            }
+            if (qrCode.ExpiryDate < DateTime.Now)
+            {
+                throw new BadRequestException("QR Code has expired");
+            }
+
+            qrCode.IsActive = false;
+            _unitOfWork.GetRepository<Qrcode>().UpdateAsync(qrCode);
+            await _unitOfWork.CommitAsync();
+            var checkinLogs = new CheckInLog()
+            {
+                QrcodeId = qrCode.Id,
+                CheckInTime = DateTime.Now,
+                CheckedInBy = accountId,
+                CheckInLocation = qrCode.TicketOrderDetail.Ticket.Show.Location,
+                Status = "confirm",
+
+            };
+            await _unitOfWork.GetRepository<CheckInLog>().InsertAsync(checkinLogs);
+            await _unitOfWork.CommitAsync();
         }
 
-        public async Task<bool> VerifyTicketIdAsync(Guid ticketId)
-        {
-            var ticketRepository = _unitOfWork.GetRepository<Ticket>();  // Giả sử bạn có entity Ticket
-            var ticket = await ticketRepository.SingleOrDefaultAsync(t => t.Id == ticketId ,null,null);
-
-            return ticket != null;
-        }
+        
     }
 }
