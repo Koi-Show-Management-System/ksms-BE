8b94d9cb96febab967e98ea37564284aa6a684ad
Châu Thiên Ân <chauthienan01072002@gmail.com>
2025-02-27 15:40:28 +0700

    get detail for order

---
+++ KSMS.Domain/Dtos/Responses/TicketOrder/GetOrderDetailResponse.cs
@@ -0,0 +1,14 @@
+﻿using KSMS.Domain.Dtos.Responses.TicketType;
+
+namespace KSMS.Domain.Dtos.Responses.TicketOrder;
+
+public class GetOrderDetailResponse
+{
+    public Guid Id { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public TicketTypeGetOrderDetailResponse TicketType { get; set; } = null!;
+}
---
+++ KSMS.Domain/Dtos/Responses/TicketType/TicketTypeGetOrderDetailResponse.cs
@@ -0,0 +1,10 @@
+﻿using KSMS.Domain.Dtos.Responses.KoiShow;
+
+namespace KSMS.Domain.Dtos.Responses.TicketType;
+
+public class TicketTypeGetOrderDetailResponse
+{
+    public string Name { get; set; } = null!;
+    
+    public KoiShowGetOrderDetailResponse? KoiShow { get; set; }
+}
