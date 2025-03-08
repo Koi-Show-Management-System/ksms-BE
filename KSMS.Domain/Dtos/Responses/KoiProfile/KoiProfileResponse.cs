49924ff63c03a93c1f95ca77c696db6af78027c8
Châu Thiên Ân <chauthienan01072002@gmail.com>
2025-03-08 16:27:10 +0700

    fix get registration page

--- KSMS.Domain/Dtos/Responses/KoiProfile/KoiProfileResponse.cs
+++ KSMS.Domain/Dtos/Responses/KoiProfile/KoiProfileResponse.cs
@@ -8,17 +8,10 @@ namespace KSMS.Domain.Dtos.Responses.KoiProfile
 {
     public class KoiProfileResponse
     {
-        public string? Name { get; set; }
-
-        public decimal? Size { get; set; }
-
-        public int? Age { get; set; }
-
+        public string? Name { get; set; }  
         public string? Gender { get; set; }
 
         public string? Bloodline { get; set; }
-
-        public string? Status { get; set; }
         
     }
 }
