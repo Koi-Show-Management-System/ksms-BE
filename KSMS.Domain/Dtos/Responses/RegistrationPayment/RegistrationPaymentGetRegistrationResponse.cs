2338e10f708c61144d704458892697289e3fe3d5
Châu Thiên Ân <chauthienan01072002@gmail.com>
2025-03-19 15:21:39 +0700

    add registration payment to registration history

---
+++ KSMS.Domain/Dtos/Responses/RegistrationPayment/RegistrationPaymentGetRegistrationResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace KSMS.Domain.Dtos.Responses.RegistrationPayment;
+
+public class RegistrationPaymentGetRegistrationResponse
+{
+    public Guid Id { get; set; }  
+    public decimal PaidAmount { get; set; }
+        
+    public DateTime PaymentDate { get; set; }
+    public string? QrcodeData { get; set; }
+    public string? PaymentMethod { get; set; }
+    public string? TransactionCode { get; set; }
+
+    public string? Status { get; set; }
+}
